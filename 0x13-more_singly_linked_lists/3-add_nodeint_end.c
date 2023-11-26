33795a7e70014bf146441b7bac9828dfcc92e5c9
Stanley Ibeneme <stanleyibeneme92@gmail.com>
2023-11-26 07:32:44 -0800

    correct

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,7 +5,7 @@
  * @head: pointer to the head of the list
  * @n: number to be used as content
  *
- * Return: address of the newly added node
+ * Return: address of the newly added node 
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
